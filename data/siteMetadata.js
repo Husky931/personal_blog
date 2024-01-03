adf87583ce30284992d018eaabb3271da5044d55
Xz33M Hask <pecevgligor@protonmail.com>
2024-01-03 18:34:15 +0700

    update sitemeta

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,20 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: `Gligor's Blog`,
+  author: 'Pecev Gligor',
   headerTitle: `Gligor's Blog`,
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Place for personal thoughts and ideas/resume',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  // mastodon: 'https://mastodon.social/@mastodonuser',
+  email: 'pecevgligor@gmail.com',
+  github: 'https://github.com/husky931',
+  twitter: 'https://twitter.com/raiusamG',
+  linkedin: 'https://linkedin.com/in/gligor-pecev',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
