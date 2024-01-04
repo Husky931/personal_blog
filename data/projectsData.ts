28c232aa063465f5ac9ba2472810fcfd48035ff6
Xz33M Hask <pecevgligor@protonmail.com>
2024-01-04 18:29:45 +0700

    finish projects page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,40 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Custom ChatGPT double app',
+    description: `Web app + electron app chatGPT app sharing a real-time synchronized database. Project done for a chain of STEM schools, used by thousands of users.`,
+    imgSrc: '/static/images/projects/gpt.png',
+    href: 'Next.js, Node.js, Express, Strapi, AWS',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Job aggregator',
+    description: `Job aggregator website with CMS, admin panel (for employers / job seekers). Job posts, email resume arrivals, backend panel for job status changes.
+    The website had automated scrapers set up to scrape and update the website daily (written in Python and automated with cron jobs).`,
+    imgSrc: '/static/images/projects/job_aggregator.png',
+    href: 'NextJS, Node.js, Python, Express, Strapi, AWS, cron jobs',
+  },
+  {
+    title: 'Global coffee trading platform',
+    description: `Full on startup project, from wireframe to production. A platform for coffee traders and brewers to connect and trade coffee. `,
+    imgSrc: '/static/images/projects/cex.png',
+    href: 'Svelte, Node.js, Postgres, AWS',
+  },
+  {
+    title: 'Custom forum app',
+    description: `Custom forum app with real-time chat and notifications.`,
+    imgSrc: '/static/images/projects/forum.png',
+    href: 'NextJS, Node.js, Express, Strapi, AWS',
+  },
+  {
+    title: 'Mobile App',
+    description: `Mini mobile app for a client to a previous employer to showcase image creation using stable diffusion.`,
+    imgSrc: '/static/images/projects/waic.png',
+    href: 'React Native, stable diffusion, API',
+  },
+  {
+    title: 'Sandbox',
+    description: `Frontend work for Codesandbox-like web app for previos employer.`,
+    imgSrc: '/static/images/projects/sandbox.png',
+    href: 'React',
   },
 ]
 
