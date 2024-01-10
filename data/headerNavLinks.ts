7a8cae68f0d9a957643fcc188b4c93046892d4c7
Xz33M Hask <pecevgligor@protonmail.com>
2024-01-10 17:35:48 +0700

    remove blog

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  // { href: '/blog', title: 'Blog' },
   { href: '/tech', title: 'Tech' },
   { href: '/projects', title: 'Projects' },
   { href: '/contact', title: 'Contact' },
